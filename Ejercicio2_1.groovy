4624cdf602f060f86c1f0cc2e76c75e24a766800
RuthCruz20 <shuth.1417@gmail.com>
2018-09-03 11:05:29 -0300

    Esquema

--- Ejercicio2_1.groovy
+++ Ejercicio2_1.groovy
@@ -1,15 +1,63 @@
 class Estudiante{
-    String nombre
+    String nombre = "yo";
     def dni
     int edad
     String ciudadNatal
     def calificacion
-    static main(args){
-    
-    }
+   
 }
 
 class Curso extends Estudiante{
+    //ejercicio 1
+    
+    
+    //ejercicio2
+    
+    
+    //ejercicio 3
+    
+    
+    //ejercicio 4
+    
+    
+    //ejercicio 5
+    
+    def estudiantesAprobados(){
+        "hello $nombre"
+    }
+    //ejercicio 6
+    
+    //ejercicio7
+    
+    
+    //ejercicio8
+    
+    
+    //ejercicio9
+    
+    //ejercicio10
+    
+    
+    //ejercicio 11
+    
+    
+    //ejercicio 12
+    
+    
+    //ejercicio 13
+    
+    
+    //ejercicio 14
+    
+    
+    //ejercicio 15
+    
+    
     
     
 }
+ static main(args){
+        def prueba = new Curso()
+        println prueba.estudiantesAprobados()
+    }
+
