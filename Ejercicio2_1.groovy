305d1ec7ca32180252f4352260fde3ae3323f100
“AllyOR <ali15.orellan@gmail.com>
2018-09-06 16:16:56 -0300

    Ejercicio2_2_2

--- Ejercicio2_1.groovy
+++ Ejercicio2_1.groovy
@@ -22,15 +22,18 @@ class Curso{
     this.lista.each{it.calificacion=0}
     }
     //ejercicio2
-    void agregarEstudiante(listaCurso, unEstudiante){
-         listaCurso.add(unEstudiante)
+    void agregarEstudiante(unEstudiante){
+         this.lista.add(unEstudiante)
+    }  
+    
+    def printLista(){
+        print "Iteracion dentro de la lista = "+" "
+        for(i = 0; i < lista.size(); i++){
+             print lista.get(i).getCiudadNatal() + " "
+        }
+        println ""
+        println ''
     }
-<<<<<<< HEAD
-    //ejercicio 4
-    
-    //ejercicio 5   
-=======
-    
     //ejercicio 3
     def cantidadDeEstudiantesInscritos(){
         this.lista.size()
@@ -43,7 +46,6 @@ class Curso{
     
     //ejercicio 5   
 
->>>>>>> 365066fcf3747aaa6542ed23524a2dfc2a970a37
     def estudiantesAprobados(lista){
         def aprobados =[]
         int i=0,l=this.cantidadDeEstudiantesInscritos(lista)
@@ -147,63 +149,54 @@ class Curso{
 }
  static main(args){
         def curso1 = new Curso()
-        def listaCurso = []
-        def estudiante1 = new Estudiante('a_1',123,12,'Oruro',5)
-        def estudiante2 = new Estudiante('a_2',222,22,'Catamarca',3)
+        def estudiante3 = new Estudiante('a_1',123,12,'Oruro',5)
+        /*def estudiante2 = new Estudiante('a_2',222,22,'Catamarca',3)
         def estudiante3 = new Estudiante('a_3',321,23,'Tucuman',5)
         def estudiante8 = new Estudiante('a_8',321,23,'Tucuman',5)
         def estudiante9 = new Estudiante('a_9',321,23,'Tucuman',6)
         def estudiante10 = new Estudiante('a_10',321,23,'Tucuman',3)
-        def estudiante11 = new Estudiante('a_11',321,23,'Tucuman',10)
+        def estudiante11 = new Estudiante('a_11',321,23,'Tucuman',10)*/
        
-        println estudiante2.getNombre()
+        //println estudiante2.getNombre()
         
-        listaCurso += [estudiante1,estudiante2,estudiante8,estudiante9,estudiante10,estudiante11]
-        println listaCurso.get(0).getNombre()
+        /*listaCurso += [estudiante1,estudiante2,estudiante8,estudiante9,estudiante10,estudiante11]
+        println listaCurso.get(0).getNombre()*/
         
         //resetearNotas
         //curso1.resetearNotas()
         
         //agregarEstudiante
-        curso1.agregarEstudiante(listaCurso, estudiante3)
+        curso1.agregarEstudiante(estudiante3)
         def estudiante4 = new Estudiante('a_4',344,19,'Salta',5)
-        curso1.agregarEstudiante(listaCurso, estudiante4)
-        print "Iteracion dentro de la lista = "+" "
+        curso1.agregarEstudiante(estudiante4)
+         def estudiante5 = new Estudiante('a_4',344,19,'Salta',5)
+        curso1.agregarEstudiante(estudiante5)
+        /*print "Iteracion dentro de la lista = "+" "
         for(i = 0; i < listaCurso.size(); i++){
              print listaCurso.get(i).getCiudadNatal() + " "
         }
         println ""
         println ''
-        println listaCurso.get(3).getNombre()
+        println listaCurso.get(3).getNombre()*/
 
         //Cantidad Estudiantes
         curso1.cantidadDeEstudiantesInscritos()
         
         //Estudiantes
-        println "Estudiantes: " + curso1.estudiantes(listaCurso)
+        //println "Estudiantes: " + curso1.estudiantes(listaCurso)
         
-        
-        
-        curso1.porcentajeDeAprobados(listaCurso)
-<<<<<<< HEAD
+        //curso1.porcentajeDeAprobados(listaCurso)
+        //curso1.existeEstudianteConNotaDiez(listaCurso) 
 
-        curso1.existeEstudianteConNotaDiez(listaCurso) 
-
-        curso1.existeEstudianteConNotaDiez(listaCurso)
-=======
-        curso1.existeEstudianteConNotaDiez(listaCurso) 
->>>>>>> 365066fcf3747aaa6542ed23524a2dfc2a970a37
+        //curso1.existeEstudianteConNotaDiez(listaCurso)
+        //curso1.existeEstudianteConNotaDiez(listaCurso) 
         
         //curso1.promedioDeCalificaciones(listaCurso)
-        curso1.unDesastre(listaCurso)
+        //curso1.unDesastre(listaCurso)
         
-<<<<<<< HEAD
-        
-=======
         /*/11 no catamarqueños, devuelve lista de Estudiante
         curso1.estudiantesNoCatamarquenios(listaCurso).each{println it}*/
         
         //12 retorna la nota con mas frecuencia, ademas el metodo calcula la frecuencia
         //curso1.calificacionMasFrecuente(listaCurso)
->>>>>>> 365066fcf3747aaa6542ed23524a2dfc2a970a37
     }
